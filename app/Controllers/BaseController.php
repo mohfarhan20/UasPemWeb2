6b5bd15e9266d1b918c77a4529e9bb6e527e2a3b
mohfarhan20 <ghifarifarhan20@gmail.com>
2021-11-27 19:15:51 +0700

    menambahkan variabel global uasModel

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -49,5 +49,6 @@ class BaseController extends Controller
 
         // E.g.: $this->session = \Config\Services::session();
         $this->nama = 'farhan';
+        // $this->userModel = new \App\Models\UserModel();
     }
 }
