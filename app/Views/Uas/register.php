03716e7c2a1c7414287556a9b63082706b931414
mohfarhan20 <ghifarifarhan20@gmail.com>
2021-11-28 10:58:28 +0700

    deal with data still eror

--- app/Views/Uas/register.php
+++ app/Views/Uas/register.php
@@ -9,7 +9,7 @@ if (isset($_SESSION["login"])) {
         </script>";
 }
 
-require 'functions.php';
+require "functions.php";
 
 if (isset($_POST["register"])) {
 
@@ -32,56 +32,55 @@ if (isset($_POST["register"])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Login AND Register</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
-    <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/3.0.1/iconfont/material-icons.min.css">
-    <link rel="stylesheet" href="assets/css/Account-setting-or-edit-profile.css">
-    <link rel="stylesheet" href="assets/css/Login-with-overlay-image.css">
-    <link rel="stylesheet" href="assets/css/Profile-with-data-and-skills.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/3.0.1/iconfont/material-icons.min.css">
+    <link rel="stylesheet" href="<?= base_url(); ?> assets/css/Account-setting-or-edit-profile.css">
+    <link rel="stylesheet" href="<?= base_url(); ?> assets/css/Login-with-overlay-image.css">
+    <link rel="stylesheet" href="<?= base_url(); ?> assets/css/Profile-with-data-and-skills.css">
     <style>
-    body {
-        background-color: #CAF0F8;
-    }
+        body {
+            background-color: #CAF0F8;
+        }
 
-    .container .header {
-        margin-top: 10px;
-        text-align: center;
-        /* warna 1 */
-        color: #023E8A;
-    }
+        .container .header {
+            margin-top: 10px;
+            text-align: center;
+            /* warna 1 */
+            color: #023E8A;
+        }
 
-    .container .box {
+        .container .box {
 
-        width: 650px;
-        height: 500px;
-        margin: auto;
-        background: #407BA7;
-        border-radius: 50px;
-    }
+            width: 650px;
+            height: 500px;
+            margin: auto;
+            background: #407BA7;
+            border-radius: 50px;
+        }
 
-    .container .box h2 {
-        padding-top: 10px;
-        text-align: center;
-        color: #FFFFFF;
-    }
+        .container .box h2 {
+            padding-top: 10px;
+            text-align: center;
+            color: #FFFFFF;
+        }
 
-    .container .box form {
-        padding: 30px;
-    }
+        .container .box form {
+            padding: 30px;
+        }
 
-    .container .box form label {
-        color: #FFFFFF;
-    }
+        .container .box form label {
+            color: #FFFFFF;
+        }
 
-    /* .container .box form button {
+        /* .container .box form button {
             position: relative;
             bottom: 20%;
             left: 40%;
             
         } */
-    .container h5 {
-        margin-top: 10px;
-        text-align: center;
-    }
+        .container h5 {
+            margin-top: 10px;
+            text-align: center;
+        }
     </style>
 </head>
 
@@ -95,33 +94,29 @@ if (isset($_POST["register"])) {
         <div class="box shadow">
             <h2>Fill the form registration below</h2>
 
-            <form action="" method="POST">
+            <form action="/Uas/functions" method="POST">
+                <?= csrf_field(); ?>
                 <div class="mb-3 ">
                     <label for="nama" class="form-label">Nama</label>
-                    <input type="text" class="form-control me-2 pe-5 ps-5" name="nama" id="nama"
-                        placeholder="input here">
+                    <input type="text" class="form-control me-2 pe-5 ps-5" name="nama" id="nama" placeholder="input here">
                 </div>
                 <div class="mb-3 ">
                     <label for="username" class="form-label">Username</label>
-                    <input type="text" class="form-control me-2 pe-5 ps-5" name="username" id="username"
-                        placeholder="input here">
+                    <input type="text" class="form-control me-2 pe-5 ps-5" name="username" id="username" placeholder="input here">
                 </div>
                 <div class="mb-3 ">
                     <label for="password" class="form-label">Password</label>
-                    <input type="password" class="form-control me-2 pe-5 ps-5" name="password" id="password"
-                        placeholder="input here">
+                    <input type="password" class="form-control me-2 pe-5 ps-5" name="password" id="password" placeholder="input here">
                 </div>
                 <div class="mb-3 ">
                     <label for="email" class="form-label">Email</label>
-                    <input type="text" class="form-control me-2 pe-5 ps-5" name="email" id="email"
-                        placeholder="input here">
+                    <input type="text" class="form-control me-2 pe-5 ps-5" name="email" id="email" placeholder="input here">
                 </div>
                 <button type="submit" name="register" class="btn btn-success center">Submit</button>
             </form>
         </div>
 
-        <p class="text-muted text-center mt-3 mb-0">Already hava an account?<a href="login.php"
-                class="text-primary ml-1">Login</a></p>
+        <p class="text-muted text-center mt-3 mb-0">Already hava an account?<a href="/Uas/login" class="text-primary ml-1">Login</a></p>
 
     </div>
 
