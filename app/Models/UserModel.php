b605c466ca8c64e1b647685be510508642d9b288
mohfarhan20 <ghifarifarhan20@gmail.com>
2021-12-01 20:32:08 +0700

    add primary key

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -7,6 +7,7 @@ use CodeIgniter\Model;
 class UserModel extends Model
 {
     protected $table = 'user';
+    protected $primaryKey = 'username';
     protected $userTimestamps = true;
     protected $allowedFields = ['nama', 'username', 'password', 'email'];
 }
